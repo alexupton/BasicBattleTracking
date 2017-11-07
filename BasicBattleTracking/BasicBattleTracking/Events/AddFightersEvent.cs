045d878a45061cff41870f7ce808f1dab6612fc9
alexupton <alexander.upton@ndsu.edu>
2017-11-07 16:00:19 -0600

    Visual Changes
    Some Visual changes for next release. Everything else can go

---
+++ BasicBattleTracking/BasicBattleTracking/Events/AddFightersEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicBattleTracking.Events
+{
+    public class AddFightersEvent : EncounterEvent
+    {
+        public List<Fighter> FighterDropList { get; set; }
+        public override int StartTurnID { get; set; }
+        public override int EndTurnID { get; set; }
+    
+        public AddFightersEvent()
+        {
+            StartTurnID = 1;
+            EndTurnID = -1;
+        }
+
+        public override void Execute()
+        {
+
+        }
+    }
+}
---
+++ BasicBattleTracking/BasicBattleTracking/Events/EncounterEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicBattleTracking.Events
+{
+    public abstract class EncounterEvent
+    {
+        public abstract int StartTurnID { get; set; }
+        public abstract int EndTurnID { get; set; }
+        public abstract void Execute();
+    }
+}
